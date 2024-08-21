becc9617d03818cd4cc46d5d8a3b5f169a4b5be4
Oskar <95188470+Tier1Coder@users.noreply.github.com>
2024-08-21 16:11:33 +0200

    [O] Changed wsgi's DJANGO_SETTINGS_MODULE

--- backend/rflow_django/core/wsgi.py
+++ backend/rflow_django/core/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.rflow_django.core.settings')
 
 application = get_wsgi_application()
