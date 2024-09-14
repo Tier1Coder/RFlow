5bb3ddafe3e602cef25b250d69dc67664538b2cd
Oskar <95188470+Tier1Coder@users.noreply.github.com>
2024-09-14 23:42:47 +0200

    [#20] Depicted icon

---
+++ frontend/rflow_react/src/assets/visualization/bpmn-shapes/gateways/ExclusiveGatewayIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ExclusiveGatewayIcon = () => (
+  <svg
+    width="100%"
+    height="100%"
+    preserveAspectRatio="none"
+    viewBox="0 0 512 512"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="106"
+      y="106"
+      width="300"
+      height="300"
+      rx="20"
+      transform="rotate(45 256 256)"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="40"
+    />
+  </svg>
+);
+
+export default ExclusiveGatewayIcon;
