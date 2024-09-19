741d04084c3730618e821d45d66473b07d0dc411
Oskar <95188470+Tier1Coder@users.noreply.github.com>
2024-09-19 17:55:53 +0200

    Added build script

---
+++ backend/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install required Python packages
+pip install -r backend/requirements.txt
+
+# Run migrations
+python backend/rflow_django/manage.py migrate
+
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER == "True" ]];
+then
+  python rflow_django/manage.py createsuperuser --no-input
+fi
