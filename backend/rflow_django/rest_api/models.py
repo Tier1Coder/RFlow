3ebd09fa2b4a041b4908a2c4a98519036e62a745
Oskar <95188470+Tier1Coder@users.noreply.github.com>
2024-10-12 19:28:15 +0200

    Created overloaded save method in BPMNDiagram model

--- backend/rflow_django/rest_api/models.py
+++ backend/rflow_django/rest_api/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+import os
+
+
+def upload_to(instance: 'BPMNDiagram', filename: str) -> str:
+    base, extension = os.path.splitext(filename)
+    new_filename = f"{instance.name}_{instance.id}{extension}"
+    return new_filename
 
 
 class BPMNDiagram(models.Model):
@@ -11,6 +18,7 @@ class BPMNDiagram(models.Model):
         file (FileField): The file associated with the BPMN diagram.
         objects (Manager): The default manager for the model.
     """
+    id = models.AutoField(primary_key=True)
     name = models.CharField(
         blank=True,  # True for custom validation (see serializers.py)
         unique=False,  # False for custom validation (see serializers.py)
@@ -18,9 +26,23 @@ class BPMNDiagram(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     file = models.FileField(
         max_length=9999,  # Max length is set to a different value (see serializers.py)
-        blank=True  # True for custom validation (see serializers.py)
+        blank=True,  # True for custom validation (see serializers.py)
+        upload_to=upload_to
     )
     objects = models.Manager()
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        """ Overloaded save method to handle file renaming using following pattern: {name}_{id}{extension}. """
+        if not self.id and self.file:
+            temp_file = self.file
+            self.file = None
+            super().save(*args, **kwargs)
+            self.file = temp_file
+
+            # Ensure updating the existing instance, not creating a new one
+            kwargs['force_insert'] = False
+
+        super().save(*args, **kwargs)
